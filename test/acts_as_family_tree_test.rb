39b480d2ef03e55342bf9eca17923b2fed75abd2
Andres Montano <amontano@virginia.edu>
2013-01-18 18:17:31 -0600

    putting the acts_as_family tree plugin into its own repository

---
+++ test/acts_as_family_tree_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ActsAsFamilyTreeTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, ActsAsFamilyTree
+  end
+end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '09c128c5742b044a44bf98c4736f63c48f847942e72e92804d352999178755c77549f944a60aa1545afc2e2eb8b9b5912ab8279160917c8cd0f44e0df95a867d'
