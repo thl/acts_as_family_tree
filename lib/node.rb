732196e30647154ed13c325aa24cad874f128002
Andres Montano <amontano@virginia.edu>
2013-02-01 21:30:49 -0600

    fixing conditions on relations

--- lib/node.rb
+++ lib/node.rb
@@ -23,8 +23,8 @@ module MM::Acts::FamilyTree::Node
       has_many :parent_relations, :class_name=>config[:tree_class], :foreign_key=>:child_node_id, :dependent=>:destroy, :extend=>config[:extensions][:parent_relations], :conditions => config[:conditions]
       has_many :child_relations, :class_name=>config[:tree_class], :foreign_key=>:parent_node_id, :dependent=>:destroy, :extend=>config[:extensions][:child_relations], :conditions => config[:conditions]
       
-      has_many :parents, :class_name=>name, :through=>:parent_relations, :source=>:parent_node, :extend=>config[:extensions][:parents], :conditions => config[:conditions]
-      has_many :children, :class_name=>name, :through=>:child_relations, :source=>:child_node, :extend=>config[:extensions][:children], :conditions => config[:conditions]
+      has_many :parents, :class_name=>name, :through=>:parent_relations, :source=>:parent_node, :extend=>config[:extensions][:parents] #, :conditions => config[:conditions]
+      has_many :children, :class_name=>name, :through=>:child_relations, :source=>:child_node, :extend=>config[:extensions][:children] #, :conditions => config[:conditions]
       
       class_eval <<-EOV
         
