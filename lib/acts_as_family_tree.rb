11a52e307b2bd8a44394bd82d4ca02ffc1ff05ef
Andres Montano <amontano@virginia.edu>
2023-08-22 16:28:24 -0600

    updating to rails 6.1

--- lib/acts_as_family_tree.rb
+++ lib/acts_as_family_tree.rb
@@ -2,9 +2,11 @@
 require 'acts_as_family_tree/active_record/acts/family_tree/ancestors_and_descendants'
 require 'acts_as_family_tree/active_record/acts/family_tree/tree'
 require 'acts_as_family_tree/active_record/acts/family_tree/node'
-require 'acts_as_family_tree/active_record/acts/family_tree/extension'
 
-ActiveRecord::Base.send :include, ActiveRecord::Acts::FamilyTree::Extension
+ActiveSupport.on_load(:active_record) do
+  require 'acts_as_family_tree/active_record/acts/family_tree/extension'
+  include ActiveRecord::Acts::FamilyTree::Extension
+end
 
 module ActsAsFamilyTree
 end
