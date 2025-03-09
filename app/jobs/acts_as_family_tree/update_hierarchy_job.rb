a6c8202a44c1eaf9e439400d8ca84eddbe14aa19
Andres Montano <amontano@virginia.edu>
2025-03-09 14:09:36 -0600

    making sure update hierarchy is just added once

--- app/jobs/acts_as_family_tree/update_hierarchy_job.rb
+++ app/jobs/acts_as_family_tree/update_hierarchy_job.rb
@@ -1,7 +1,20 @@
 module ActsAsFamilyTree
   class UpdateHierarchyJob < ApplicationJob
-    queue_as :default
-        
+    queue_as :update_hierarchy
+    
+    around_enqueue do |job, block|
+      object = job.arguments.first
+      previous = Delayed::Job.where(queue: UpdateHierarchyJob.queue_name, reference: object).first
+      if previous.nil?
+        block.call if !block.nil?
+        delayed_job = Delayed::Job.find(job.provider_job_id)
+        delayed_job.update!(reference: object)
+      else
+        priority = job.priority || 0
+        previous.update!(priority: priority) if priority < previous.priority
+      end
+    end
+    
     def perform(record)
       record.update_hierarchy
     end
--- lib/acts_as_family_tree/version.rb
+++ lib/acts_as_family_tree/version.rb
@@ -1,3 +1,3 @@
 module ActsAsFamilyTree
-  VERSION = '1.2.7'
+  VERSION = '1.2.8'
 end
